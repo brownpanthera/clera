292ae1ab64d0ff400613cd9580e252a6cd902883
brownpanthera <alifaizan1983@gmail.com>
2025-02-15 23:36:32 +0530

    remove logs

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const { spawnSync } = require('child_process');
 function clearTerminal() {
     const command = process.platform === 'win32' ? 'cmd' : 'clear';
     const args = process.platform === 'win32' ? ['/c', 'cls'] : [];
-    console.log(command)
-    console.log(args)
     
-    // spawnSync(command, args, { stdio: 'inherit' });
+    spawnSync(command, args, { stdio: 'inherit' });
 }
 
 clearTerminal();
