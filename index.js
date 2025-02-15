519823a63cb9a08a5dcdf1a2a08625439e2b0665
brownpanthera <alifaizan1983@gmail.com>
2025-02-15 23:21:50 +0530

    windows fix

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
+
 const { spawnSync } = require('child_process');
 
 function clearTerminal() {
-    const command = process.platform === 'win32' ? 'cls' : 'clear';
-    spawnSync(command, { stdio: 'inherit' });
+    const command = process.platform === 'win32' ? 'cmd' : 'clear';
+    const args = process.platform === 'win32' ? ['/c', 'cls'] : [];
+    console.log(command)
+    console.log(args)
+    
+    // spawnSync(command, args, { stdio: 'inherit' });
 }
 
 clearTerminal();
